6e1d5765706475012c3a2fc71420dab3a8592196
Anna Calini <calinianna@gmail.com>
2018-01-15 23:20:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,6 +15,8 @@ function logWhisper(string) {
 }
 
 function sayHiToGrandma(string){
+  var string.toLowerCase ="hello"
+  var string.toUpperCase = "HELLO"
 if(string === string.toLowerCase) {
   return "I can\'t hear you!"
   } else if(string === string.toUpperCase) {
